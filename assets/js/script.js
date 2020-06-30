5b6e7a6a260842e700839d7728918180f9972d8b
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-07-01 00:35:58 +0530

    Version 4 with AOS

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+(function($) {
+	'use strict';
+
+	//Navigation
+
+	var app = (function() {
+		var body = undefined;
+		var menu = undefined;
+		var menuItems = undefined;
+		var init = function init() {
+			body = document.querySelector('body');
+			menu = document.querySelector('.menu-icon');
+			menuItems = document.querySelectorAll('.nav__list-item');
+			applyListeners();
+		};
+		var applyListeners = function applyListeners() {
+			menu.addEventListener('click', function() {
+				return toggleClass(body, 'nav-active');
+			});
+		};
+		var toggleClass = function toggleClass(element, stringClass) {
+			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);
+			else element.classList.add(stringClass);
+		};
+		init();
+	})();
+})(jQuery);
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+	if (e.target.checked) {
+		document.documentElement.setAttribute('data-theme', 'light');
+		const emText = (document.getElementById('emText').innerHTML = 'Dark Mode');
+	} else {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		const emText = (document.getElementById('emText').innerHTML = 'Light Mode');
+	}
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
